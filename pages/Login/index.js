fa66da8f8bbea005f452670304f6cd9b8c697c78
AugustBemsen <danireptor7@gmail.com>
2021-06-24 18:40:13 +0100

    initialize login

---
+++ pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Login = () => {
+  return (
+    <>
+      <LoginStyles>login works</LoginStyles>
+    </>
+  );
+};
+
+export default Login;
+const LoginStyles = styled.div``;
